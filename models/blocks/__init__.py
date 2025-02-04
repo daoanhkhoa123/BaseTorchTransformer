e4e9fe153c90b4f0b5a05f06b94d3eca14e8fe4d
daoanhkhoa123 <158530319+daoanhkhoa123@users.noreply.github.com>
2025-02-04 10:00:02 +0700

    Delete models directory

--- models/blocks/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Hello
-My name is Dao Anh Khoa
-I am an AI engineer
-I am coding transformer
-"""
--- models/embedding/TokenEmbedding.py
+++
@@ -1,6 +0,0 @@
-from torch import nn
-
-
-class TokenEmbedding(nn.Embedding):
-    def __init__(self, vocab_size, d_model, padding_idx=1):
-        super().__init__(vocab_size, d_model, padding_idx)
